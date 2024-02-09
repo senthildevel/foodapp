b1a95c69399bdb9a21bf09b5bb5ffe66a3b1f23a
senthildevel <132339613+senthildevel@users.noreply.github.com>
2024-02-09 19:48:17 +0530

    Added index file

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports.add = (a,b) => {
+
+    let c = a + b;
+    return c;
+}
+
+module.exports.sub = (a,b) => {
+
+    let c = a + b;
+    return c;
+}
+
+module.exports.div = (a,b) => {
+
+    let c = a + b;
+    return c;
+}
+
+
+
